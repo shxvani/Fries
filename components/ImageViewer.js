9c367579e723fa269622ecaefdc170bcf6f3896c
asxwin <ashwinvelu2003@gmail.com>
2023-02-25 17:04:14 +0530

    imp

--- components/ImageViewer.js
+++ components/ImageViewer.js
@@ -4,23 +4,15 @@ import { Video } from 'expo-av';
 
 export default function ImageViewer({ placeholderImageSource, selectedImage }) {
   const imageSource =
-    selectedImage !== null ? { uri: "https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4" } : placeholderImageSource;
+    selectedImage !== null ? { uri:selectedImage } : placeholderImageSource;
 
-  return  <Video
-            ref={video}
-            style={styles.video}
-            source={{
-                uri: 'https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
-            }}
-            useNativeControls
-            resizeMode="contain"
-            isLooping
-            onPlaybackStatusUpdate={status => setStatus(() => status)}
+  return  <Image
+    source={imageSource} style={styles.image}
     />;
 }
 
 const styles = StyleSheet.create({  
-  video: {
+  image: {
     width: 320,
     height: 440,
     borderRadius: 18,
